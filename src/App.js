a15b891d4a5be9378e1ca9559080f4d79d464d9b
Joe McNally <joe.t.mcnally@hotmail.com>
2022-07-03 17:50:34 +0100

    Finish the landing page
    Finish designing the landing page. Just need to sort out the footer
    sticking to the bottom and not overlapping the content.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import pic from "./title-pic.jpg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Welcome to Pacman!</h1>
+      <img className="title-pic" src={pic} />
+      <div className="register">
+        <label className="label" htmlFor="name-input">
+          Enter your name:
+        </label>
+        <input
+          className="input"
+          id="name-input"
+          type="text"
+          placeholder="Name"
+        ></input>
+        <button className="submit-button">Submit</button>
+      </div>
+      <footer>
+        <p className="footer-text">
+          Please don&apos;t threaten to sue me Bandai Namco. I&apos;m making
+          this for my portfolio. No money is being made from this project.
+          Don&apos;t be like Nintendo.
+        </p>
+      </footer>
     </div>
   );
 }
-
-export default App;
