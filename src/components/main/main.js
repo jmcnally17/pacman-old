98e814be71e8f546b68cf1195df1102454c5d07e
Joe McNally <joe.t.mcnally@hotmail.com>
2022-07-04 16:53:23 +0100

    Begin Play page
    Modify the input field and button on the landing page to require a name
    and then render the Play component on click.
    
    Add the title "Let's Play" and a div which is where the game board will
    be.

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import pic from "./title-pic.jpg";
 import "./main.css";
+import Play from "./play/play";
 
 export default function Main() {
+  const [name, setName] = useState("");
+
+  const handleName = ({ target }) => {
+    setName(target.value);
+    console.log(name);
+  };
+
+  const handleSubmit = () => {
+    if (name === "") {
+      window.alert("You must enter a name");
+    } else {
+      const mainEl = ReactDOM.createRoot(document.getElementById("main"));
+      mainEl.render(<Play />);
+    }
+  };
+
   return (
-    <div className="main">
-      <h1>Welcome to Pac-Man!</h1>
-      <img className="title-pic" src={pic} />
-      <div className="register">
-        <label className="label" htmlFor="name-input">
-          Enter your name:
-        </label>
-        <input
-          className="input-name"
-          id="name-input"
-          type="text"
-          placeholder="Name"
-        ></input>
-        <button className="submit-button">Submit</button>
+    <div className="main" id="main">
+      <div id="login">
+        <h1>Welcome to Pac-Man!</h1>
+        <img className="title-pic" src={pic} />
+        <div className="register">
+          <label className="label" htmlFor="name-input">
+            Enter your name:
+          </label>
+          <input
+            className="input-name"
+            id="name-input"
+            type="text"
+            placeholder="Name"
+            onChange={handleName}
+          ></input>
+          <button className="submit-button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/main/play/play.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./play.css";
+
+export default function Play() {
+  return (
+    <div>
+      <h1>Let&apos;s Play!</h1>
+      <div className="map">This is where the game will be</div>
+    </div>
+  );
+}
