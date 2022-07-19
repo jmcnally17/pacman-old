dcec48678ace5445793d09947859661bd71452d6
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-19 21:01:07 +0100

    Add change player button
    Add another button to the leaderboard page that will reload the page
    and send the player back to the original Main page where they enter a
    name.

--- client/src/components/main/leaderboard/leaderboard.js
+++ client/src/components/main/leaderboard/leaderboard.js
@@ -7,11 +7,16 @@ export default function Leaderboard({ score, mainEl, name }) {
     mainEl.render(<Game name={name} mainEl={mainEl} />);
   };
 
+  const handleChangePlayer = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="leaderboard">
       <h1>Game Over</h1>
       <h4>You scored {score.points} points</h4>
       <button onClick={handlePlayAgain}>Play Again</button>
+      <button onClick={handleChangePlayer}>Change Player</button>
     </div>
   );
 }
