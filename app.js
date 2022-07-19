36c23579b1dd6441d709291ba5f106b3313996e0
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-19 01:18:16 +0100

    Delete cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
