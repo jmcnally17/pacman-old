27c6024190ca30ea628ae74caad8052d0680638b
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-21 19:12:42 +0100

    Slow ghosts when scared
    Slightly decrease the ghosts speed when they are scared from 2.5 to 2.
    Unfortunately, the speed cannot be reduced any further or the ghost
    movement mechanics will break, making them move through boundaries.
    
    This may be due to framerates on the canvas but unsure.

--- client/src/components/main/game/scripts/powerUps/scareGhost.js
+++ client/src/components/main/game/scripts/powerUps/scareGhost.js
@@ -1,6 +1,8 @@
 export default function scareGhost(ghost) {
   ghost.changeScaredState();
+  ghost.speed = 2;
   ghost.scaredTimeout = setTimeout(() => {
     ghost.changeScaredState();
+    ghost.speed = 2.5;
   }, 5000);
 }
