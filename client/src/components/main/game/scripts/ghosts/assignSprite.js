eef7ecc9b8417b483c23c32e805e89ffeba31e2b
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-21 17:15:38 +0100

    Update sprites constantly
    Add more conditionals to assignSprite to now constantly update the sprite
    of the ghost for every animation frame and to account for every type of
    ghost sprite, except for the scaredGhostWhite sprite. This is because
    an animation will be needed in order to incorporate it.

--- client/src/components/main/game/scripts/ghosts/assignSprite.js
+++ client/src/components/main/game/scripts/ghosts/assignSprite.js
@@ -1,5 +1,47 @@
 export default function assingSprite(ghost) {
   if (ghost.isScared) {
     ghost.image.src = "./images/scaredGhostBlue.png";
+  } else {
+    if (ghost.colour === "red") {
+      if (ghost.velocity.x > 0) {
+        ghost.image.src = "./images/redGhostRight.png";
+      } else if (ghost.velocity.x < 0) {
+        ghost.image.src = "./images/redGhostLeft.png";
+      } else if (ghost.velocity.y < 0) {
+        ghost.image.src = "./images/redGhostUp.png";
+      } else if (ghost.velocity.y > 0) {
+        ghost.image.src = "./images/redGhostDown.png";
+      }
+    } else if (ghost.colour === "pink") {
+      if (ghost.velocity.x > 0) {
+        ghost.image.src = "./images/pinkGhostRight.png";
+      } else if (ghost.velocity.x < 0) {
+        ghost.image.src = "./images/pinkGhostLeft.png";
+      } else if (ghost.velocity.y < 0) {
+        ghost.image.src = "./images/pinkGhostUp.png";
+      } else if (ghost.velocity.y > 0) {
+        ghost.image.src = "./images/pinkGhostDown.png";
+      }
+    } else if (ghost.colour === "cyan") {
+      if (ghost.velocity.x > 0) {
+        ghost.image.src = "./images/cyanGhostRight.png";
+      } else if (ghost.velocity.x < 0) {
+        ghost.image.src = "./images/cyanGhostLeft.png";
+      } else if (ghost.velocity.y < 0) {
+        ghost.image.src = "./images/cyanGhostUp.png";
+      } else if (ghost.velocity.y > 0) {
+        ghost.image.src = "./images/cyanGhostDown.png";
+      }
+    } else if (ghost.colour === "orange") {
+      if (ghost.velocity.x > 0) {
+        ghost.image.src = "./images/orangeGhostRight.png";
+      } else if (ghost.velocity.x < 0) {
+        ghost.image.src = "./images/orangeGhostLeft.png";
+      } else if (ghost.velocity.y < 0) {
+        ghost.image.src = "./images/orangeGhostUp.png";
+      } else if (ghost.velocity.y > 0) {
+        ghost.image.src = "./images/orangeGhostDown.png";
+      }
+    }
   }
 }
