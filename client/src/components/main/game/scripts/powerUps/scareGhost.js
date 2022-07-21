578d3a289c88dcc993d6f15d505beab06acb5cca
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-21 20:01:26 +0100

    Don't slow the ghosts down
    Remove the lines in scareGhost.js that slow the ghost down due to a bug
    where the ghosts will move out of bounds when they move through the tunnel.
    
    This may be due to the ghosts position when switching between the normal
    and slow speeds as the difference between them is half a pixel.

--- client/src/components/main/game/scripts/powerUps/scareGhost.js
+++ client/src/components/main/game/scripts/powerUps/scareGhost.js
@@ -1,8 +1,6 @@
 export default function scareGhost(ghost) {
   ghost.changeScaredState();
-  ghost.speed = 2;
   ghost.scaredTimeout = setTimeout(() => {
     ghost.changeScaredState();
-    ghost.speed = 2.5;
   }, 5000);
 }
