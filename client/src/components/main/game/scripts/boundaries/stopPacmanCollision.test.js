c8af49b6bb5cd85a257fd2689bc3b565fab2314b
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-23 21:33:13 +0100

    Add tests for stopPacmanCollision

---
+++ client/src/components/main/game/scripts/boundaries/stopPacmanCollision.test.js
@@ -0,0 +1,53 @@
+import stopPacmanCollision from "./stopPacmanCollision";
+
+describe("stopPacmanCollision", () => {
+  it("changes Pac-Man's velocity to 0 in both directions when colliding with a boundary", () => {
+    const mockPacman = {
+      position: {
+        x: 200,
+        y: 200,
+      },
+      velocity: {
+        x: 5,
+        y: 5,
+      },
+      radius: 40,
+    };
+    const mockBoundary = {
+      position: {
+        x: 200,
+        y: 200,
+      },
+      height: 50,
+      width: 50,
+    };
+    stopPacmanCollision(mockBoundary, mockPacman);
+    expect(mockPacman.velocity.x).toBe(0);
+    expect(mockPacman.velocity.y).toBe(0);
+  });
+
+  it("leaves Pac-Man's velocity unchanged when not colliding with a boundary", () => {
+    const mockPacman = {
+      position: {
+        x: 350,
+        y: 40,
+      },
+      velocity: {
+        x: 5,
+        y: 5,
+      },
+      radius: 10,
+    };
+    const mockBoundary = {
+      position: {
+        x: 200,
+        y: 200,
+      },
+      height: 10,
+      width: 10,
+    };
+    stopPacmanCollision(mockBoundary, mockPacman);
+    expect(mockPacman.velocity.x).toBe(5);
+    expect(mockPacman.velocity.y).toBe(5);
+  });
+});
