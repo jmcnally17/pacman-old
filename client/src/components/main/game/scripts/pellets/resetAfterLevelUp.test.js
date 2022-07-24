be3225ea17d8b2a12c4d5b7f38f7ba1287684e98
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-24 01:06:27 +0100

    Make reset level test

---
+++ client/src/components/main/game/scripts/pellets/resetAfterLevelUp.test.js
@@ -0,0 +1,52 @@
+import resetAfterLevelUp from "./resetAfterLevelUp";
+
+describe("resetAfterLevelUp", () => {
+  it("calls all the necessary functions to reset the board", () => {
+    const mockPacman = {
+      reset: () => undefined,
+    };
+    const mockLastKeyPressed = {
+      key: "up",
+    };
+    const mockGhost = {
+      reset: () => undefined,
+      isScared: true,
+      changeScaredState: () => undefined,
+      scaredTimeout: setTimeout(() => undefined, 5000),
+    };
+    const mockPellet = {
+      changeEatenState: () => undefined,
+    };
+    const mockPowerUp = {
+      hasBeenEaten: true,
+      changeEatenState: () => undefined,
+    };
+    const mockGhosts = [mockGhost, mockGhost, mockGhost];
+    const mockPellets = [mockPellet, mockPellet];
+    const mockPowerUps = [mockPowerUp];
+    const pacmanResetSpy = jest.spyOn(mockPacman, "reset");
+    const ghostResetSpy = jest.spyOn(mockGhost, "reset");
+    const changeScaredStateSpy = jest.spyOn(mockGhost, "changeScaredState");
+    const pelletChangeEatenStateSpy = jest.spyOn(
+      mockPellet,
+      "changeEatenState"
+    );
+    const powerUpChangeEatenStateSpy = jest.spyOn(
+      mockPowerUp,
+      "changeEatenState"
+    );
+    resetAfterLevelUp(
+      mockPacman,
+      mockLastKeyPressed,
+      mockGhosts,
+      mockPellets,
+      mockPowerUps
+    );
+    expect(pacmanResetSpy).toHaveBeenCalledTimes(1);
+    expect(mockLastKeyPressed.key).toBe("");
+    expect(ghostResetSpy).toHaveBeenCalledTimes(3);
+    expect(changeScaredStateSpy).toHaveBeenCalledTimes(3);
+    expect(pelletChangeEatenStateSpy).toHaveBeenCalledTimes(2);
+    expect(powerUpChangeEatenStateSpy).toHaveBeenCalledTimes(1);
+  });
+});
