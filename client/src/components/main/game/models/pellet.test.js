9ed4d134283517d4f4d07c9fb1d0145598788f90
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-24 22:20:22 +0100

    Finish tests for Pellet

--- client/src/components/main/game/models/pellet.test.js
+++ client/src/components/main/game/models/pellet.test.js
@@ -1,27 +1,52 @@
 import Pellet from "./pellet";
 
+let pellet;
+
 describe("Pellet", () => {
-  it("has a number of instance variables upon instantiation", () => {
-    const pellet = new Pellet({
+  beforeEach(() => {
+    pellet = new Pellet({
       position: {
         x: 50,
         y: 100,
       },
     });
-    expect(pellet.radius).toBe(2);
-    expect(pellet.hasBeenEaten).toBe(false);
-    expect(pellet.position.x).toBe(50);
-    expect(pellet.position.y).toBe(100);
   });
 
-  it("changeEatenState can change the pellet to being eaten", () => {
-    const pellet = new Pellet({
-      position: {
-        x: 50,
-        y: 100,
-      },
+  describe("upon instantiation", () => {
+    it("has a number of instance variables", () => {
+      expect(pellet.position.x).toBe(50);
+      expect(pellet.position.y).toBe(100);
+      expect(pellet.radius).toBe(2);
+      expect(pellet.hasBeenEaten).toBe(false);
     });
-    pellet.changeEatenState();
-    expect(pellet.hasBeenEaten).toBe(true);
+  });
+
+  describe("changeEatenState", () => {
+    it("can change the pellet to being eaten", () => {
+      pellet.changeEatenState();
+      expect(pellet.hasBeenEaten).toBe(true);
+    });
+  });
+
+  describe("draw", () => {
+    it("calls the necessary functions on ctx to draw the pellet", () => {
+      const mockCtx = {
+        beginPath: () => undefined,
+        arc: () => undefined,
+        fillStyle: "",
+        fill: () => undefined,
+        closePath: () => undefined,
+      };
+      const beginPathSpy = jest.spyOn(mockCtx, "beginPath");
+      const arcSpy = jest.spyOn(mockCtx, "arc");
+      const fillSpy = jest.spyOn(mockCtx, "fill");
+      const closePathSpy = jest.spyOn(mockCtx, "closePath");
+      pellet.draw(mockCtx);
+      expect(beginPathSpy).toHaveBeenCalledTimes(1);
+      expect(arcSpy).toHaveBeenCalledTimes(1);
+      expect(fillSpy).toHaveBeenCalledTimes(1);
+      expect(closePathSpy).toHaveBeenCalledTimes(1);
+      expect(mockCtx.fillStyle).toBe("white");
+    });
   });
 });
