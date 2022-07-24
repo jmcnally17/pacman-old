737ac315c2b03179ec72f53b023c41b45a61ead6
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-24 22:12:53 +0100

    Finish tests for Boundary

--- client/src/components/main/game/models/boundary.test.js
+++ client/src/components/main/game/models/boundary.test.js
@@ -1,27 +1,50 @@
 import Boundary from "./boundary";
 
+let boundary;
+let mockImage;
+
 describe("Boundary", () => {
-  it("always has a width and height of 40", () => {
-    const boundary = new Boundary({
+  beforeEach(() => {
+    mockImage = {
+      src: "./randomSource",
+    };
+    boundary = new Boundary({
       position: {
         x: 40,
         y: 100,
       },
+      image: mockImage,
     });
-    expect(boundary.width).toBe(20);
-    expect(boundary.height).toBe(20);
   });
 
-  it("has a position that is passed in on instantiation", () => {
-    const boundary = new Boundary({
-      position: {
+  describe("upon instantiation", () => {
+    it("has a width and height of 20", () => {
+      expect(boundary.width).toBe(20);
+      expect(boundary.height).toBe(20);
+    });
+
+    it("has a position that is passed in on instantiation", () => {
+      expect(boundary.position).toEqual({
         x: 40,
         y: 100,
-      },
+      });
     });
-    expect(boundary.position).toEqual({
-      x: 40,
-      y: 100,
+
+    it("has an image that is passed in on instantiation", () => {
+      expect(boundary.image).toEqual({
+        src: "./randomSource",
+      });
+    });
+  });
+
+  describe("draw", () => {
+    it("calls drawImage on ctx to draw the boundary", () => {
+      const mockCtx = {
+        drawImage: () => undefined,
+      };
+      const ctxSpy = jest.spyOn(mockCtx, "drawImage");
+      boundary.draw(mockCtx);
+      expect(ctxSpy).toHaveBeenCalledTimes(1);
     });
   });
 });
