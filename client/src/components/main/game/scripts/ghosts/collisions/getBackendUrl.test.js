9912673af6e4706c4652cc1e126ea71aec34878c
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-07-29 21:19:03 +0100

    Refactor
    Extract a new function for checking the backend url to fetch from when
    making the POST request to save a score to the database.
    
    Use jest-fetch-mock to also mock an error for fetch so that the test
    coverage for saveScore will be raised to 100%.

---
+++ client/src/components/main/game/scripts/ghosts/collisions/getBackendUrl.test.js
@@ -0,0 +1,17 @@
+import getBackendUrl from "./getBackendUrl";
+
+describe("getBackendUrl", () => {
+  it("returns the live site backend url if it exists", () => {
+    const mockReactAppUrl = "https://randomsite.com";
+    expect(getBackendUrl(mockReactAppUrl)).toBe(
+      "https://randomsite.com/backend/scores"
+    );
+  });
+
+  it("returns the localhost url backend if the live site url does not exist", () => {
+    const mockReactAppUrl = undefined;
+    expect(getBackendUrl(mockReactAppUrl)).toBe(
+      "http://localhost:9000/backend/scores"
+    );
+  });
+});
