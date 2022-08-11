5c8fa242589ae26ed3499fdf36389f64021f50ba
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-11 15:00:29 +0100

    Make start
    Create the start function for the CycleTimer class that sets the timeout
    with a delay of 7 seconds or 20 seconds, depending on the value of this.count.

--- client/src/components/game/models/cycleTimer.js
+++ client/src/components/game/models/cycleTimer.js
@@ -1,6 +1,19 @@
-export default class cycleTimer {
+export default class CycleTimer {
   constructor() {
     this.timeout = null;
     this.count = 0;
+  }
+
+  start(ghosts) {
+    this.timeout = setTimeout(
+      () => {
+        ghosts.forEach((ghost) => {
+          ghost.changeHuntingState();
+        });
+        this.start(ghosts);
+      },
+      this.count === 0 ? 7000 : 20000
+    );
+    this.count === 0 ? this.count++ : this.count--;
   }
 }
--- client/src/components/game/models/cycleTimer.test.js
+++ client/src/components/game/models/cycleTimer.test.js
@@ -1,11 +1,41 @@
-import cycleTimer from "./cycleTimer";
+import CycleTimer from "./cycleTimer";
 
-describe("Timer", () => {
+let timer;
+let mockGhost;
+let mockGhosts;
+let huntingSpy;
+jest.useFakeTimers();
+
+describe("CycleTimer", () => {
+  beforeEach(() => {
+    timer = new CycleTimer();
+    mockGhost = {
+      changeHuntingState: () => true,
+    };
+    mockGhosts = [mockGhost, mockGhost];
+    huntingSpy = jest.spyOn(mockGhost, "changeHuntingState");
+  });
+
   describe("upon instanstation", () => {
     it("has a number of instance variables", () => {
-      const timer = new cycleTimer();
       expect(timer.timeout).toBeNull();
       expect(timer.count).toBe(0);
+    });
+  });
+
+  describe("start", () => {
+    it("calls setTimeout with a time of seven seconds when the count is 0 and adds 1 to the count", () => {
+      jest.spyOn(global, "setTimeout");
+      timer.start(mockGhosts);
+
+      expect(setTimeout).toHaveBeenCalledTimes(1);
+      expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 7000);
+      expect(timer.timeout).not.toBeNull();
+      expect(timer.count).toBe(1);
+
+      jest.runOnlyPendingTimers();
+      expect(huntingSpy).toHaveBeenCalledTimes(2);
+      expect(setTimeout).toHaveBeenCalledTimes(2);
     });
   });
 });
