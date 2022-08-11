940e9757ebc79bf485ceea8ecd4064af4326815b
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-11 01:37:22 +0100

    Refactor
    Shorten startHuntingInterval by using the quarternary operator for both
    the setTimeout time and the conditional of whether to add one to the count
    or take one away.

--- client/src/components/game/mechanics/startHuntingInterval.js
+++ client/src/components/game/mechanics/startHuntingInterval.js
@@ -1,19 +1,12 @@
 export default function startHuntingInterval(ghosts, count, huntingTimeout) {
-  if (count.number === 0) {
-    huntingTimeout.timeout = setTimeout(() => {
+  huntingTimeout.timeout = setTimeout(
+    () => {
       ghosts.forEach((ghost) => {
         ghost.changeHuntingState();
       });
       startHuntingInterval(ghosts, count, huntingTimeout);
-    }, 7000);
-    count.number++;
-  } else if (count.number === 1) {
-    huntingTimeout.timeout = setTimeout(() => {
-      ghosts.forEach((ghost) => {
-        ghost.changeHuntingState();
-      });
-      startHuntingInterval(ghosts, count, huntingTimeout);
-    }, 20000);
-    count.number--;
-  }
+    },
+    count.number === 0 ? 7000 : 20000
+  );
+  count.number === 0 ? count.number++ : count.number--;
 }
