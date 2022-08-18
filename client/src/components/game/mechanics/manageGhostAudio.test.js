992844775b8f5dcfa17b2ef0150e95fb1f7f6fc6
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-18 15:52:33 +0100

    Play the siren audio
    Add another conditional to manageGhostAudio that plays the siren audio if
    the scared timer is not running and make another test to check it leaves the
    scared audio playing if the scared timer is running.

--- client/src/components/game/mechanics/manageGhostAudio.test.js
+++ client/src/components/game/mechanics/manageGhostAudio.test.js
@@ -10,8 +10,11 @@ describe("manageGhostAudio", () => {
     mockSirenAudio = {
       playing: () => true,
       pause: () => undefined,
+      play: () => undefined,
     };
     mockScaredAudio = {
+      playing: () => false,
+      pause: () => undefined,
       play: () => undefined,
     };
     mockGhostAudioObjects = [mockSirenAudio, mockScaredAudio];
@@ -31,8 +34,11 @@ describe("manageGhostAudio", () => {
     mockSirenAudio = {
       playing: () => true,
       pause: () => undefined,
+      play: () => undefined,
     };
     mockScaredAudio = {
+      playing: () => false,
+      pause: () => undefined,
       play: () => undefined,
     };
     mockGhostAudioObjects = [mockSirenAudio, mockScaredAudio];
@@ -46,5 +52,53 @@ describe("manageGhostAudio", () => {
     expect(mockSirenAudio.playing).toHaveBeenCalledTimes(0);
     expect(mockSirenAudio.pause).toHaveBeenCalledTimes(0);
     expect(mockScaredAudio.play).toHaveBeenCalledTimes(0);
+  });
+
+  it("plays the siren audio if the scared timer is not running and the scared audio is playing", () => {
+    mockSirenAudio = {
+      playing: () => false,
+      pause: () => undefined,
+      play: () => undefined,
+    };
+    mockScaredAudio = {
+      playing: () => true,
+      pause: () => undefined,
+      play: () => undefined,
+    };
+    mockGhostAudioObjects = [mockSirenAudio, mockScaredAudio];
+    mockScaredTimer = {
+      isRunning: false,
+    };
+    jest.spyOn(mockScaredAudio, "playing");
+    jest.spyOn(mockScaredAudio, "pause");
+    jest.spyOn(mockSirenAudio, "play");
+    manageGhostAudio(mockGhostAudioObjects, mockScaredTimer);
+    expect(mockScaredAudio.playing).toHaveBeenCalledTimes(1);
+    expect(mockScaredAudio.pause).toHaveBeenCalledTimes(1);
+    expect(mockSirenAudio.play).toHaveBeenCalledTimes(1);
+  });
+
+  it("leaves the scared audio playing if the scared timer is running", () => {
+    mockSirenAudio = {
+      playing: () => false,
+      pause: () => undefined,
+      play: () => undefined,
+    };
+    mockScaredAudio = {
+      playing: () => true,
+      pause: () => undefined,
+      play: () => undefined,
+    };
+    mockGhostAudioObjects = [mockSirenAudio, mockScaredAudio];
+    mockScaredTimer = {
+      isRunning: true,
+    };
+    jest.spyOn(mockScaredAudio, "playing");
+    jest.spyOn(mockScaredAudio, "pause");
+    jest.spyOn(mockSirenAudio, "play");
+    manageGhostAudio(mockGhostAudioObjects, mockScaredTimer);
+    expect(mockScaredAudio.playing).toHaveBeenCalledTimes(0);
+    expect(mockScaredAudio.pause).toHaveBeenCalledTimes(0);
+    expect(mockSirenAudio.play).toHaveBeenCalledTimes(0);
   });
 });
