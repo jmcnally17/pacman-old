df1e6af4b2a8ef1ce93cbd03f1d9ceac16708233
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-18 15:45:56 +0100

    Make new audio test
    Create another test that checks that the siren audio is not pause if the
    scared timer is not running.

--- client/src/components/game/mechanics/manageGhostAudio.test.js
+++ client/src/components/game/mechanics/manageGhostAudio.test.js
@@ -26,4 +26,25 @@ describe("manageGhostAudio", () => {
     expect(mockSirenAudio.pause).toHaveBeenCalledTimes(1);
     expect(mockScaredAudio.play).toHaveBeenCalledTimes(1);
   });
+
+  it("leaves the siren audio playing if the scared timer is not running", () => {
+    mockSirenAudio = {
+      playing: () => true,
+      pause: () => undefined,
+    };
+    mockScaredAudio = {
+      play: () => undefined,
+    };
+    mockGhostAudioObjects = [mockSirenAudio, mockScaredAudio];
+    mockScaredTimer = {
+      isRunning: false,
+    };
+    jest.spyOn(mockSirenAudio, "playing");
+    jest.spyOn(mockSirenAudio, "pause");
+    jest.spyOn(mockScaredAudio, "play");
+    manageGhostAudio(mockGhostAudioObjects, mockScaredTimer);
+    expect(mockSirenAudio.playing).toHaveBeenCalledTimes(0);
+    expect(mockSirenAudio.pause).toHaveBeenCalledTimes(0);
+    expect(mockScaredAudio.play).toHaveBeenCalledTimes(0);
+  });
 });
