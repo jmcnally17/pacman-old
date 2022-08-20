e3c333558b9b59ae9f3b7324880bea6fcbc5bd02
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-20 12:13:30 +0100

    Draw the pellets and powerUps
    Call draw and update on the pellets and power ups as long as they have not
    yet been eaten.

--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
@@ -1,6 +1,18 @@
-export default function runDeathAnimation(variables, ctx, boundaries) {
+export default function runDeathAnimation(
+  variables,
+  ctx,
+  boundaries,
+  pellets,
+  powerUps
+) {
   cancelAnimationFrame(variables.animationId);
   requestAnimationFrame(runDeathAnimation);
   ctx.clearRect(0, 0, 896, 992);
   boundaries.forEach((boundary) => boundary.draw(ctx));
+  pellets.forEach((pellet) => {
+    if (!pellet.hasBeenEaten) pellet.draw(ctx);
+  });
+  powerUps.forEach((powerUp) => {
+    if (!powerUp.hasBeenEaten) powerUp.update(ctx);
+  });
 }
--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
@@ -4,6 +4,14 @@ let mockVariables;
 let mockCtx;
 let mockBoundary;
 let mockBoundaries;
+let mockPellet;
+let mockPellets;
+let mockEatenPellet;
+let mockEatenPellets;
+let mockPowerUp;
+let mockPowerUps;
+let mockEatenPowerUp;
+let mockEatenPowerUps;
 
 describe("runDeathAnimation", () => {
   beforeEach(() => {
@@ -17,11 +25,37 @@ describe("runDeathAnimation", () => {
       draw: () => undefined,
     };
     mockBoundaries = [mockBoundary, mockBoundary];
+    mockPellet = {
+      hasBeenEaten: false,
+      draw: () => undefined,
+    };
+    mockPellets = [mockPellet, mockPellet];
+    mockEatenPellet = {
+      hasBeenEaten: true,
+      draw: () => undefined,
+    };
+    mockEatenPellets = [mockEatenPellet, mockEatenPellet];
+    mockPowerUp = {
+      hasBeenEaten: false,
+      update: () => undefined,
+    };
+    mockPowerUps = [mockPowerUp, mockPowerUp];
+    mockEatenPowerUp = {
+      hasBeenEaten: true,
+      update: () => undefined,
+    };
+    mockEatenPowerUps = [mockEatenPowerUp, mockEatenPowerUp];
   });
 
   it("cancels the current animation frame", () => {
     jest.spyOn(global, "cancelAnimationFrame");
-    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
     expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
     expect(cancelAnimationFrame).toHaveBeenCalledWith(
       mockVariables.animationId
@@ -30,23 +64,93 @@ describe("runDeathAnimation", () => {
 
   it("calls requestAnimationFrame on itself", () => {
     jest.spyOn(global, "requestAnimationFrame");
-    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
     expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
     expect(requestAnimationFrame).toHaveBeenCalledWith(runDeathAnimation);
   });
 
   it("calls clearRect on the ctx with the board dimensions", () => {
     jest.spyOn(mockCtx, "clearRect");
-    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
     expect(mockCtx.clearRect).toHaveBeenCalledTimes(1);
     expect(mockCtx.clearRect).toHaveBeenCalledWith(0, 0, 896, 992);
   });
 
   it("calls draw on all the boundaries", () => {
     jest.spyOn(mockBoundary, "draw");
-    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
     expect(mockBoundary.draw).toHaveBeenCalledTimes(2);
     expect(mockBoundary.draw).toHaveBeenNthCalledWith(1, mockCtx);
     expect(mockBoundary.draw).toHaveBeenNthCalledWith(2, mockCtx);
+  });
+
+  it("calls draw on the pellets that have not been eaten", () => {
+    jest.spyOn(mockPellet, "draw");
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
+    expect(mockPellet.draw).toHaveBeenCalledTimes(2);
+    expect(mockPellet.draw).toHaveBeenNthCalledWith(1, mockCtx);
+    expect(mockPellet.draw).toHaveBeenNthCalledWith(2, mockCtx);
+  });
+
+  it("does not call draw on the pellets that have been eaten", () => {
+    jest.spyOn(mockEatenPellet, "draw");
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockEatenPellets,
+      mockPowerUps
+    );
+    expect(mockEatenPellet.draw).toHaveBeenCalledTimes(0);
+  });
+
+  it("calls update on the power ups that have not been eaten", () => {
+    jest.spyOn(mockPowerUp, "update");
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockPellets,
+      mockPowerUps
+    );
+    expect(mockPowerUp.update).toHaveBeenCalledTimes(2);
+    expect(mockPowerUp.update).toHaveBeenNthCalledWith(1, mockCtx);
+    expect(mockPowerUp.update).toHaveBeenNthCalledWith(2, mockCtx);
+  });
+
+  it("does not call update on the power ups that have been eaten", () => {
+    jest.spyOn(mockEatenPowerUp, "update");
+    runDeathAnimation(
+      mockVariables,
+      mockCtx,
+      mockBoundaries,
+      mockEatenPellets,
+      mockEatenPowerUps
+    );
+    expect(mockEatenPowerUp.update).toHaveBeenCalledTimes(0);
   });
 });
