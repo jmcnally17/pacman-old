ee63de3c1f8408e694040ad9a5d069902cd23521
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-20 11:59:14 +0100

    Call clearRect

--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
@@ -1,4 +1,5 @@
-export default function runDeathAnimation(variables) {
+export default function runDeathAnimation(variables, ctx) {
   cancelAnimationFrame(variables.animationId);
   requestAnimationFrame(runDeathAnimation);
+  ctx.clearRect(0, 0, 896, 992);
 }
--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
@@ -1,17 +1,21 @@
 import runDeathAnimation from "./runDeathAnimation";
 
 let mockVariables;
+let mockCtx;
 
 describe("runDeathAnimation", () => {
   beforeEach(() => {
     mockVariables = {
       animationId: 1956382,
     };
+    mockCtx = {
+      clearRect: () => undefined,
+    };
   });
 
   it("cancels the current animation frame", () => {
     jest.spyOn(global, "cancelAnimationFrame");
-    runDeathAnimation(mockVariables);
+    runDeathAnimation(mockVariables, mockCtx);
     expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
     expect(cancelAnimationFrame).toHaveBeenCalledWith(
       mockVariables.animationId
@@ -20,8 +24,15 @@ describe("runDeathAnimation", () => {
 
   it("calls requestAnimationFrame on itself", () => {
     jest.spyOn(global, "requestAnimationFrame");
-    runDeathAnimation(mockVariables);
+    runDeathAnimation(mockVariables, mockCtx);
     expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
     expect(requestAnimationFrame).toHaveBeenCalledWith(runDeathAnimation);
+  });
+
+  it("calls clearRect on the ctx with the board dimensions", () => {
+    jest.spyOn(mockCtx, "clearRect");
+    runDeathAnimation(mockVariables, mockCtx);
+    expect(mockCtx.clearRect).toHaveBeenCalledTimes(1);
+    expect(mockCtx.clearRect).toHaveBeenCalledWith(0, 0, 896, 992);
   });
 });
