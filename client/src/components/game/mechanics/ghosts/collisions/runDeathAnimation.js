a8162940225dea422b6e1b471360b4125ccaceb7
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-20 12:04:19 +0100

    Draw all the boundaries

--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.js
@@ -1,5 +1,6 @@
-export default function runDeathAnimation(variables, ctx) {
+export default function runDeathAnimation(variables, ctx, boundaries) {
   cancelAnimationFrame(variables.animationId);
   requestAnimationFrame(runDeathAnimation);
   ctx.clearRect(0, 0, 896, 992);
+  boundaries.forEach((boundary) => boundary.draw(ctx));
 }
--- client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
+++ client/src/components/game/mechanics/ghosts/collisions/runDeathAnimation.test.js
@@ -2,6 +2,8 @@ import runDeathAnimation from "./runDeathAnimation";
 
 let mockVariables;
 let mockCtx;
+let mockBoundary;
+let mockBoundaries;
 
 describe("runDeathAnimation", () => {
   beforeEach(() => {
@@ -11,11 +13,15 @@ describe("runDeathAnimation", () => {
     mockCtx = {
       clearRect: () => undefined,
     };
+    mockBoundary = {
+      draw: () => undefined,
+    };
+    mockBoundaries = [mockBoundary, mockBoundary];
   });
 
   it("cancels the current animation frame", () => {
     jest.spyOn(global, "cancelAnimationFrame");
-    runDeathAnimation(mockVariables, mockCtx);
+    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
     expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
     expect(cancelAnimationFrame).toHaveBeenCalledWith(
       mockVariables.animationId
@@ -24,15 +30,23 @@ describe("runDeathAnimation", () => {
 
   it("calls requestAnimationFrame on itself", () => {
     jest.spyOn(global, "requestAnimationFrame");
-    runDeathAnimation(mockVariables, mockCtx);
+    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
     expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
     expect(requestAnimationFrame).toHaveBeenCalledWith(runDeathAnimation);
   });
 
   it("calls clearRect on the ctx with the board dimensions", () => {
     jest.spyOn(mockCtx, "clearRect");
-    runDeathAnimation(mockVariables, mockCtx);
+    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
     expect(mockCtx.clearRect).toHaveBeenCalledTimes(1);
     expect(mockCtx.clearRect).toHaveBeenCalledWith(0, 0, 896, 992);
+  });
+
+  it("calls draw on all the boundaries", () => {
+    jest.spyOn(mockBoundary, "draw");
+    runDeathAnimation(mockVariables, mockCtx, mockBoundaries);
+    expect(mockBoundary.draw).toHaveBeenCalledTimes(2);
+    expect(mockBoundary.draw).toHaveBeenNthCalledWith(1, mockCtx);
+    expect(mockBoundary.draw).toHaveBeenNthCalledWith(2, mockCtx);
   });
 });
