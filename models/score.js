5c6bb5e776f59c58ec1b42e3b9151f892b448791
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-08-28 12:21:46 +0100

    Create migration
    Create a migration for the Score model which can be run to create the table
    in the necessary database.

---
+++ models/score.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Score extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Score.init({
+    name: DataTypes.STRING,
+    points: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Score',
+  });
+  return Score;
+};
