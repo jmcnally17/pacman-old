ae3a5097194bc0727427dd87fea9578b31f07533
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-30 12:01:19 +0000

    Add pause instruction
    On the main component, add some text into the instructions to let the player
    know they can pause and unpause the game by pressing the esc key.

--- client/src/components/main/main.test.js
+++ client/src/components/main/main.test.js
@@ -36,8 +36,9 @@ describe("Main", () => {
     const instructionsEl = screen.getByText(
       "Use the directional keys to move Pac-Man around the board while avoiding the ghosts as best you can. " +
         "Pick up a power up and then attack the ghosts! " +
-        "Eat all the pellets on the board to level up. " +
-        "(For mobile and tablet users, a D-pad will appear below the board for you to move Pac-Man around)"
+        "Eat all the pellets on the board to level up " +
+        "(For mobile and tablet users, a D-pad will appear below the board for you to move Pac-Man around). " +
+        "Press esc to pause and unpause the game at any time."
     );
     expect(instructionsEl).toBeInTheDocument();
   });
