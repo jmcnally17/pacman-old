9f349354db410e04f935ad64e2a9faf1f3382fc6
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-10-30 11:53:10 +0000

    Incorporate loadPauseOverlay
    Call the new function in the pause event listener and edit the size and
    position of the pause text.

--- client/src/components/game/mechanics/eventListeners/loadPauseText.js
+++ client/src/components/game/mechanics/eventListeners/loadPauseText.js
@@ -1,7 +1,7 @@
 export default function loadPauseText(ctx) {
   ctx.globalAlpha = 1;
-  ctx.font = "50px Arial";
+  ctx.font = "100px Arial";
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.fillText("Paused", 448, 576);
+  ctx.fillText("Paused", 448, 500);
 }
--- client/src/components/game/mechanics/eventListeners/loadPauseText.test.js
+++ client/src/components/game/mechanics/eventListeners/loadPauseText.test.js
@@ -12,10 +12,10 @@ describe("loadPauseText", () => {
     jest.spyOn(mockCtx, "fillText");
     loadPauseText(mockCtx);
     expect(mockCtx.globalAlpha).toBe(1);
-    expect(mockCtx.font).toBe("50px Arial");
+    expect(mockCtx.font).toBe("100px Arial");
     expect(mockCtx.fillStyle).toBe("white");
     expect(mockCtx.textAlign).toBe("center");
     expect(mockCtx.fillText).toHaveBeenCalledTimes(1);
-    expect(mockCtx.fillText).toHaveBeenCalledWith("Paused", 448, 576);
+    expect(mockCtx.fillText).toHaveBeenCalledWith("Paused", 448, 500);
   });
 });
