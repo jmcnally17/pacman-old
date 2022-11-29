9042eb3367ef1bcc1ec8eee3c093d079f04a7a51
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-11-29 14:30:24 +0000

    Display level
    Render the level in the game info canvas for the player. Set the text
    align to center as this will be in the middle of the canvas.

--- client/src/components/game/mechanics/display/displayLevel.test.js
+++ client/src/components/game/mechanics/display/displayLevel.test.js
@@ -1,5 +1,38 @@
 import displayLevel from "./displayLevel";
 
+let mockCtx;
+let mockVariables;
+
 describe("displayLevel", () => {
-  it("does something", () => {});
+  beforeEach(() => {
+    mockCtx = {
+      fillStyle: null,
+      textAlign: null,
+      fillText: () => undefined,
+    };
+    mockVariables = {
+      level: 7,
+    };
+  });
+
+  it("changes the ctx fillStyle to white", () => {
+    displayLevel(mockCtx, mockVariables);
+    expect(mockCtx.fillStyle).toBe("white");
+  });
+
+  it("changes the ctx textAlign to center", () => {
+    displayLevel(mockCtx, mockVariables);
+    expect(mockCtx.textAlign).toBe("center");
+  });
+
+  it("calls fillText on ctx to render the score text", () => {
+    jest.spyOn(mockCtx, "fillText");
+    displayLevel(mockCtx, mockVariables);
+    expect(mockCtx.fillText).toHaveBeenCalledTimes(1);
+    expect(mockCtx.fillText).toHaveBeenCalledWith(
+      `Level: ${mockVariables.level}`,
+      300,
+      15
+    );
+  });
 });
