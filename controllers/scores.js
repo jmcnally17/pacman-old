601ef04026e6f92297e3d13bcda16d2a7a290b88
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-22 11:49:36 +0000

    Add backend error handling

--- controllers/scores.js
+++ controllers/scores.js
@@ -20,23 +20,33 @@ process.on("exit", () => {
 
 const ScoresController = {
   Index: async (req, res) => {
-    const scoresData = await client.zRangeWithScores("scores", 0, 9, {
-      REV: true,
-    });
-    res.json({
-      scores: scoresData,
-    });
+    try {
+      const scoresData = await client.zRangeWithScores("scores", 0, 9, {
+        REV: true,
+      });
+      res.json({
+        scores: scoresData,
+      });
+    } catch {
+      res.status(500).send({ message: "scores could not be retrieved" });
+    }
   },
   Create: async (req, res) => {
-    const currentScore = await client.zScore("scores", req.body.name);
-    if (req.body.points > currentScore || currentScore === null) {
-      await client.zAdd("scores", {
-        score: req.body.points,
-        value: req.body.name,
-      });
-      res.status(201).send({ message: "your score has been saved" });
-    } else {
-      res.status(201).send({ message: "you have not beaten your high score" });
+    try {
+      const currentScore = await client.zScore("scores", req.body.name);
+      if (req.body.points > currentScore || currentScore === null) {
+        await client.zAdd("scores", {
+          score: req.body.points,
+          value: req.body.name,
+        });
+        res.status(201).send({ message: "your score has been saved" });
+      } else {
+        res
+          .status(201)
+          .send({ message: "you have not beaten your high score" });
+      }
+    } catch {
+      res.status(500).send({ message: "your score was not saved" });
     }
   },
 };
